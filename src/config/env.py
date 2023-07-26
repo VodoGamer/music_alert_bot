1e5bf6d1323358f6bfa055784ad55d2301f1b291
VodoGamer <vodogamer@yandex.ru>
2023-07-26 17:54:11 +0300

    python: change version to `3.11` 🐍

--- src/config/env.py
+++ src/config/env.py
@@ -4,17 +4,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-
-if not any((TELEGRAM_TOKEN, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DB)):
-    raise FileNotFoundError(
-        "The .env file does not have all the fields from .env.example filled in"
-    )
+POSTGRES_USER = os.getenv("POSTGRES_USER", "")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "")
 
 POSTGRES_CONNECT = (
     f"postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
