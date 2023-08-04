ad7f2f7f70d8dd1a20a941c3035fa3c4c46027e6
VodoGamer <vodogamer@yandex.ru>
2023-08-04 14:39:46 +0300

    ⚙️ refactor (add_artist)

--- src/handlers/add_artist.py
+++ src/handlers/add_artist.py
@@ -1,9 +1,8 @@
-import re
-
 from telegrinder import CallbackQuery, Dispatch, Message
-from telegrinder.rules import CallbackDataMarkup, Text
+from telegrinder.rules import CallbackDataMarkup, Regex, Text
 from telegrinder.types import InputFile
 from telegrinder.types import Message as CallbackMessage
+from yandex_music import Artist
 
 from src.client import api, gettext, logger
 from src.handlers.keyboards import get_correct_or_no_kb
@@ -23,24 +22,28 @@ async def add_artist(message: Message):
     await set_state(message.from_user.id, State.WaitArtistNickname)
 
 
+@dp.message(
+    StateMessageRule(State.WaitArtistNickname),
+    Regex(r"https:\/\/music\.yandex\.(?:com|ru)\/artist\/(\d*)\??"),
+)
+async def artist_link(message: Message, match: tuple[str]):
+    artist = await get_artist_by_id(int(match[0]))
+    if artist:
+        await send_information_about_artist(message, artist)
+
+
 @dp.message(StateMessageRule(State.WaitArtistNickname))
 async def artist_search(message: Message):
     if not message.text:
         return logger.error(f"{message=}")
-    if not message.text.startswith("https://"):
-        artists = await search_artists(message.text)
-        if not artists:
-            return await api.send_message(
-                chat_id=message.chat.id, text=gettext("artist_not_exist")
-            )
-        artist = artists[0]
-    else:
-        match = re.match(r"https:\/\/music\.yandex\.com\/artist\/(\d*)\??", message.text)
-        if not match:
-            return
-        artist = await get_artist_by_id(int(match.groups()[0]))
-        if not artist:
-            return
+    artists = await search_artists(message.text)
+    if not artists:
+        return await api.send_message(chat_id=message.chat.id, text=gettext("artist_not_exist"))
+    artist = artists[0]
+    await send_information_about_artist(message, artist)
+
+
+async def send_information_about_artist(message: Message, artist: Artist):
     if not artist.name:
         await api.send_message(chat_id=message.chat.id, text=gettext("artist_not_exist"))
     elif not artist.cover:
@@ -51,7 +54,7 @@ async def artist_search(message: Message):
         )
     else:
         await api.send_photo(
-            message.chat.id,
+            chat_id=message.chat.id,
             caption=gettext("best_result_of_artist_search").format(artist.name),
             photo=InputFile(
                 artist.name, await artist.cover.download_bytes_async(size="m1000x1000")
@@ -68,21 +71,18 @@ async def correct_artist(event: CallbackQuery, message: CallbackMessage, artist_
     await api.delete_message(chat_id=message.chat.id, message_id=message.message_id)
     answer = await api.send_message(chat_id=message.chat.id, text=gettext("albums_initializing"))
 
-    await add_artist_to_user(message.from_user.id, int(artist.id), artist.name)
-    await user_albums_init(int(artist_id), message.from_user.id)
+    await add_artist_to_user(event.from_user.id, int(artist.id), artist.name)
+    await user_albums_init(int(artist_id), event.from_user.id)
     await api.edit_message_text(
         chat_id=message.chat.id,
         message_id=answer.unwrap().message_id,
         text=gettext("user_select_new_artist").format(artist.name),
     )
-    await remove_state(message.from_user.id)
+    await remove_state(event.from_user.id)
 
 
 @dp.callback_query(CallbackDataMarkup("correct/no/<artist_id>"), CallbackHasMessageRule())
 async def wrong_artist(event: CallbackQuery, message: CallbackMessage, artist_id: str):
     await api.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-    await api.send_message(
-        chat_id=message.chat.id,
-        text=gettext("wrong_artist_search"),
-    )
-    await remove_state(message.from_user.id)
+    await api.send_message(chat_id=message.chat.id, text=gettext("wrong_artist_search"))
+    await remove_state(event.from_user.id)
