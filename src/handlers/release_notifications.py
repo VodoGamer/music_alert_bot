a2c0a20dedd35668afb6ef6463914f75c59e9164
VodoGamer <vodogamer@yandex.ru>
2023-08-04 09:42:19 +0300

    🐛 fix: if failed to get cover notification has not send

--- src/handlers/release_notifications.py
+++ src/handlers/release_notifications.py
@@ -1,12 +1,24 @@
 from telegrinder.types import InputFile
 from yandex_music import Album, Artist
+from yandex_music.exceptions import TimedOutError
 
 from src.client import api, formatter, gettext
 from src.handlers.keyboards import get_release_link_kb
 
 
 async def send_release_notification_to_user(user_id: int, release: Album, artist: Artist):
-    cover = await release.download_cover_bytes_async("m1000x1000")
+    try:
+        cover = await release.download_cover_bytes_async("m1000x1000")
+    except TimedOutError:
+        await api.send_message(
+            chat_id=user_id,
+            text=formatter(gettext("release_notification")).format(
+                artist.name, ", ".join(release.artists_name()), release.title
+            ),
+            parse_mode=formatter.PARSE_MODE,
+            reply_markup=get_release_link_kb(release),
+        )
+        return
     await api.send_photo(
         chat_id=user_id,
         photo=InputFile(release.title or "new_release", cover),
