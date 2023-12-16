babb3a02300700c930abdd18415914a50a801041
VodoGamer <vodogamer@yandex.ru>
2023-12-16 22:16:31 +0300

    🩹 remove singleton for yandex api

--- src/services/yandex/artists.py
+++ src/services/yandex/artists.py
@@ -1,10 +1,10 @@
-from yandex_music import Album, Artist, ArtistAlbums, ClientAsync
+from yandex_music import Album, Artist, ArtistAlbums
 
 from src.client import yandex_client
 
 
 async def get_artist_albums(artist_ids: list[int]) -> list[ArtistAlbums | None]:
-    api = await _get_yandex_api()
+    api = await yandex_client.init()
     artists_albums: list[ArtistAlbums | None] = []
     for artist_id in artist_ids:
         artists_albums.append(await api.artists_direct_albums(artist_id, page_size=0))
@@ -12,25 +12,18 @@ async def get_artist_albums(artist_ids: list[int]) -> list[ArtistAlbums | None]:
 
 
 async def get_albums(album_ids: list[int | str]) -> list[Album]:
-    api = await _get_yandex_api()
+    api = await yandex_client.init()
     return await api.albums(album_ids)
 
 
 async def search_artists(nickname: str) -> list[Artist] | None:
-    api = await _get_yandex_api()
+    api = await yandex_client.init()
     search_result = await api.search(nickname, type_="artist")
     if search_result and search_result.artists:
         return search_result.artists.results
 
 
 async def get_artist_by_id(id: int) -> Artist | None:
-    api = await _get_yandex_api()
+    api = await yandex_client.init()
     artists = await api.artists([id])
     return artists[0]
-
-
-async def _get_yandex_api() -> ClientAsync:
-    if not getattr(_get_yandex_api, "api", None):
-        api = await yandex_client.init()
-        _get_yandex_api.api = api
-    return _get_yandex_api.api
